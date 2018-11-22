4ad11fd264101de48214f5f88462538de05eead0
Mike Bender <mikebender@illumon.com>
2018-11-22 11:50:30 -0500

    Update so that it actually shows an editor window.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
 import * as monaco from 'monaco-editor';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.container = null;
+    this.editor = null;
+  }
+
+  componentDidMount() {
+    this.editor = monaco.editor.create(this.container);
+  }
+
+  componentWillUnmount() {
+    this.editor.dispose();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div ref={container => { this.container = container }} style={{width:500, height: 500}}/>
       </div>
     );
   }
